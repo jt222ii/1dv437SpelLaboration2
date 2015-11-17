d798bf711d24b8708d8c5a2c446573765326007e
jt222ii <jt222ii@student.lnu.se>
2015-11-17 16:49:42 +0100

    Created an offput so that the particles always spawn in the middle of the screen

--- PartiklarOchSimuleringar/PartiklarOchSimuleringar/Camera.cs
+++ PartiklarOchSimuleringar/PartiklarOchSimuleringar/Camera.cs
@@ -10,7 +10,10 @@ namespace PartiklarOchSimuleringar
     class Camera
     {
         private int sizeOfField;
+        int windowSizeX;
+        int windowSizeY;
         private float particleSizeOfField = 0.02f;
+        Vector2 offput = Vector2.Zero;
         float scale = 1;
         public Camera()
         {
@@ -18,22 +21,24 @@ namespace PartiklarOchSimuleringar
         }
         public void setSizeOfField(Viewport port)
         {
-            int windowSizeX = port.Width;
-            int windowSizeY = port.Height;
+            windowSizeX = port.Width;
+            windowSizeY = port.Height;
             if (windowSizeX < windowSizeY)
             {
                 sizeOfField = windowSizeX;
+                offput.Y = (windowSizeY - sizeOfField)/2;
             }
             else
             {
                 sizeOfField = windowSizeY;
+                offput.X = (windowSizeX - sizeOfField)/2;
             }
         }
 
         public Vector2 convertToVisualCoords(Vector2 coords, SplitterParticle particle)
         {
-            float visualX = coords.X * sizeOfField - (particle._spark.Width/2)*scale;
-            float visualY = coords.Y * sizeOfField - (particle._spark.Height / 2)*scale;
+            float visualX = coords.X * sizeOfField - (particle._spark.Width/2)*scale + offput.X;
+            float visualY = coords.Y * sizeOfField - (particle._spark.Height / 2) * scale + offput.Y;
             return new Vector2(visualX, visualY);
         }
         public float Scale(SplitterParticle particle)
